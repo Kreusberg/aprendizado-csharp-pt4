7e5f9b3faa4296b07e7448da2796c2fb82687dbe
Lucas Lodi Kreusberg <kreusberglucas@gmail.com>
2023-10-13 15:13:35 -0300

    add LINQ filters

--- ScreenSound-04/Program.cs
+++ ScreenSound-04/Program.cs
@@ -1,5 +1,6 @@
 ﻿using ScreenSound_04.Modelos;
 using System.Text.Json;
+using ScreenSound_04.Filtros;
 
 using (HttpClient client = new())
 {
@@ -7,11 +8,15 @@ using (HttpClient client = new())
     {
         string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
         var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
-        musicas[0].ExibirDetalhesDaMusica();
+        //musicas[0].ExibirDetalhesDaMusica();
+        //LinqFilter.FiltrarTodosOsGenerosMusicais(musicas);
+        //LinqOrder.ExibirListaDeArtistasOrdenados(musicas);
+        //LinqFilter.FiltrarArtistasPorGeneroMusical(musicas, "pop");
+        LinqFilter.FiltrarMusicasDeUmArtista(musicas, "Michael Jackson");
 
-    } catch (Exception ex)
+    }
+    catch (Exception ex)
     {
         Console.WriteLine($"Temos um problema: {ex.Message}");
     }
 }
-
