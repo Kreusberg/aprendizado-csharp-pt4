63bcb71c9e70cb7ed1693527fae00b2d43cc1e1b
Lucas Lodi Kreusberg <kreusberglucas@gmail.com>
2023-10-13 11:26:35 -0300

    create Musica class

--- ScreenSound-04/Program.cs
+++ ScreenSound-04/Program.cs
@@ -1,9 +1,14 @@
-﻿using (HttpClient client = new())
+﻿using ScreenSound_04.Modelos;
+using System.Text.Json;
+
+using (HttpClient client = new())
 {
     try
     {
         string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
-        Console.WriteLine(resposta);
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+        musicas[0].ExibirDetalhesDaMusica();
+
     } catch (Exception ex)
     {
         Console.WriteLine($"Temos um problema: {ex.Message}");
